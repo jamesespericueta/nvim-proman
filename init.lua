af5bab38e42c6fba58431a1c703d2605b00565e2
jamesespericueta <espericueta2303@gmail.com>
2024-10-24 19:14:35 -0400

    oops, forgot the argument logic

--- init.lua
+++ init.lua
@@ -8,22 +8,26 @@ function M.init()
     if not util.file_exists() then
         util.create_file()
     end
-    local project_list = util.load_projects()
-    if #project_list ==0 then
-        vim.defer_fn(function ()
-            require('nvim-tree.api').tree.open()
-            vim.cmd('echo "Project list empty. Please add project with :AddProject command"')
-        end, 50)
-    elseif #project_list > 0 then
-        local in_project = util.is_in_project(project_list)
-        if not in_project then
-            util.list_projects(project_list)
-        elseif in_project then
+
+    if vim.fn.argc() == 0 then
+        local project_list = util.load_projects()
+        if #project_list ==0 then
             vim.defer_fn(function ()
                 require('nvim-tree.api').tree.open()
-            end, 10)
+                vim.cmd('echo "Project list empty. Please add project with :AddProject command"')
+            end, 50)
+        elseif #project_list > 0 then
+            local in_project = util.is_in_project(project_list)
+            if not in_project then
+                util.list_projects(project_list)
+            elseif in_project then
+                vim.defer_fn(function ()
+                    require('nvim-tree.api').tree.open()
+                end, 10)
+            end
         end
     end
+
 end
 
 return M
